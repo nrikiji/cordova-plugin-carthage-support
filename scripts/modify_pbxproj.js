84630a53d072b88ed825d52b539d7d7aa6859bd3
nrikiji <nrikiji@gmail.com>
2019-01-17 04:20:53 +0900

    Update scripts

--- scripts/modify_pbxproj.js
+++ scripts/modify_pbxproj.js
@@ -10,67 +10,69 @@ var parser = new xml2js.Parser()
 var stdio = { stdio:[0, 1, 2] };
 
 module.exports = function (context) {
-  var rootPath = context.opts.projectRoot
-  var configPath = path.join(rootPath, "config.xml")
-  var configParser = getConfigParser(context, configPath)
-  var platformPath = rootPath + "/platforms/ios"
-  var cartfilePath = platformPath + "/Cartfile"
-  var xcodeProjectPath = platformPath + "/" + configParser.name() + ".xcodeproj"
+    var rootPath = context.opts.projectRoot
+    var configPath = path.join(rootPath, "config.xml")
+    var configParser = getConfigParser(context, configPath)
+    var platformPath = rootPath + "/platforms/ios"
+    var cartfilePath = platformPath + "/Cartfile"
+    var xcodeProjectPath = platformPath + "/" + configParser.name() + ".xcodeproj"
 
-  var cartfiles = []
-  var frameworks = []
+    var cartfiles = []
+    var frameworks = []
 
-  context.opts.cordova.plugins.forEach(id => {
-    parser.parseString(fs.readFileSync("plugins/" + id + "/plugin.xml"), (err, data) => {
-      if (!err && data.plugin.platform) {
-        data.plugin.platform.forEach((platform) => {
-          if (platform.$.name === "ios" && platform.carthage) {
-            platform.carthage.forEach((carthage) => {
-              if (carthage.cartfile) {
-                carthage.cartfile.forEach((cartfile) => {
-                  cartfiles.push(cartfile)
+    context.opts.cordova.plugins.forEach(id => {
+        var xmlPath = "plugins/" + id + "/plugin.xml"
+        parser.parseString(fs.readFileSync(xmlPath), (err, data) => {
+            if (err || !data.plugin.platform) {
+                return
+            }
+            data.plugin.platform.forEach((platform) => {
+                if (platform.$.name != "ios" || !platform.carthage) {
+                    return
+                }
+                platform.carthage.forEach((carthage) => {
+                    var items = carthage.cartfile || []
+                    items.forEach((cartfile) => {
+                        cartfiles.push(cartfile)
+                    })
+                    items = carthage.framework || []
+                    items.filter((framework) => {
+                        return framework.$ && framework.$.src
+                    }).forEach((framework) => {
+                        frameworks.push(framework.$.src)
+                    })
                 })
-              }
-              if (carthage.framework) {
-                carthage.framework.forEach((framework) => {
-                  if (framework.$ && framework.$.src) {
-                    frameworks.push(framework.$.src)
-                  }
-                })
-              }
             })
-          }
         })
-      }
     })
-  })
 
-  if (0 < cartfiles.length && 0 < frameworks.length) {
-    console.log("### Create Cartfile")
+    if (cartfiles.length < 1 || frameworks.length < 1) {
+        process.exit(0)
+    }
+
+    console.log("##### Create Cartfile")
     fs.writeFileSync(cartfilePath, cartfiles.join("\r\n"))
 
-    // var json = { frameworks: frameworks, project_path: xcodeProjectPath }
-    // execSync("ruby " + __dirname + "/modify_pbxproj.rb '" + JSON.stringify(json) + "'", stdio);
-
-    console.log("### Update Carthage")
-    exec("carthage update", { cwd: platformPath }, (err, stdout, stderr) => {
-      console.log(stdout)
-      console.log(stderr)
-
-      console.log("### Setting Run Script")
-      var json = { frameworks: frameworks, project_path: xcodeProjectPath }
-      execSync("ruby " + __dirname + "/modify_pbxproj.rb '" + JSON.stringify(json) + "'", stdio);
+    console.log("##### Update Carthage(>=0.20)")
+    exec("carthage bootstrap --platform iOS --cache-builds", { cwd: platformPath }, (err, stdout, stderr) => {
+        if (err) {
+            console.err(err, stderr)
+            process.exit(1)
+        }
+        console.log(stdout)
+        console.log("##### Setting Run Script")
+        var json = { frameworks: frameworks, project_path: xcodeProjectPath }
+        execSync("ruby " + __dirname + "/modify_pbxproj.rb '" + JSON.stringify(json) + "'", stdio);
     })
-  }
 
-  function getConfigParser(context, config) {
-    var semver = context.requireCordovaModule("semver")
-    var ConfigParser
-    if (semver.lt(context.opts.cordova.version, "5.4.0")) {
-      ConfigParser = context.requireCordovaModule("cordova-lib/src/ConfigParser/ConfigParser")
-    } else {
-      ConfigParser = context.requireCordovaModule("cordova-common/src/ConfigParser/ConfigParser")
+    function getConfigParser(context, config) {
+        var semver = context.requireCordovaModule("semver")
+        var ConfigParser
+        if (semver.lt(context.opts.cordova.version, "5.4.0")) {
+            ConfigParser = context.requireCordovaModule("cordova-lib/src/ConfigParser/ConfigParser")
+        } else {
+            ConfigParser = context.requireCordovaModule("cordova-common/src/ConfigParser/ConfigParser")
+        }
+        return new ConfigParser(config)
     }
-    return new ConfigParser(config)
-  }
 }
--- scripts/modify_pbxproj.rb
+++ scripts/modify_pbxproj.rb
@@ -5,6 +5,16 @@ require "json"
 
 BUILD_PHASE_NAME = "Cordova Plugin Carthage Run Script"
 
+def shell_script_phase?(build_phase)
+  if build_phase.isa != "PBXShellScriptBuildPhase"
+    false
+  elsif build_phase.name != BUILD_PHASE_NAME
+    false
+  else
+    true
+  end
+end
+
 begin
   if ARGV.size < 1
     raise "require argument"
@@ -20,71 +30,61 @@ begin
   project_path = frameworks["project_path"]
   project = Xcodeproj::Project.open(project_path)
 
+  ## Framework Search Paths
   project.targets.each do |target|
     paths = ['$(inherited)', "$(PROJECT_DIR)/Carthage/Build/iOS"]
     target.build_settings("Debug")["FRAMEWORK_SEARCH_PATHS"] = paths
     target.build_settings("Release")["FRAMEWORK_SEARCH_PATHS"] = paths
   end
 
-  # project.build_configuration_list.build_configurations.each do |build_configuration|
-  #   p "###############################"
-  #   p build_configuration.isa
-  #   p build_configuration.name
+  ## Run Script
+  project.targets.each do |target|
+    exists_build_phase = false
+    target.build_phases.each do |build_phase|
+      if shell_script_phase?(build_phase)
+        exists_build_phase = true
+      end
+    end
+    if !exists_build_phase
+      build_phase = project.new(Xcodeproj::Project::Object::PBXShellScriptBuildPhase)
+      build_phase.name = BUILD_PHASE_NAME
+      build_phase.shell_path = "/bin/sh"
+      build_phase.shell_script = "/usr/local/bin/carthage copy-frameworks\n"
+      build_phase.input_paths = []
+      target.build_phases << build_phase
+    end
+  end
 
-  #   paths = ["$(PROJECT_DIR)/Carthage/Build/iOS"]
-  #   build_configuration.build_settings["FRAMEWORK_SEARCH_PATHS"] = paths
-  # end
+  project.targets.each do |target|
+    target.build_phases.each do |build_phase|
+      if shell_script_phase?(build_phase)
+        frameworks["frameworks"].each do |framework|
+          framework_path = "$(SRCROOT)/Carthage/Build/iOS/" + framework
+          unless build_phase.input_paths.include?(framework_path)
+            build_phase.input_paths.push(framework_path)
+          end
+        end
+      end
+    end
+  end
 
-  # project.targets.each do |target|
-  #   flagBuildPhase = false
-  #   target.build_phases.each do |build_phase|
-  #     if build_phase.isa != "PBXShellScriptBuildPhase"
-  #       next
-  #     elsif build_phase.name != BUILD_PHASE_NAME
-  #       next
-  #     else
-  #       flagBuildPhase = true
-  #     end
-  #   end
-  #   if !flagBuildPhase
-  #     build_phase = project.new(Xcodeproj::Project::Object::PBXShellScriptBuildPhase)
-  #     build_phase.name = BUILD_PHASE_NAME
-  #     build_phase.shell_path = "/bin/sh"
-  #     build_phase.shell_script = "/usr/local/bin/carthage copy-frameworks\n"
-  #     build_phase.input_paths = []
-  #     target.build_phases << build_phase
-  #   end
-  # end
+  ## Linked Frameworks and Libraries
+  project.targets.each do |target|
+    target.build_phases.each do |build_phase|
+      if build_phase.isa == "PBXFrameworksBuildPhase"
+        frameworks["frameworks"].each do |framework|
+          framework_ref = project.new(Xcodeproj::Project::Object::PBXFileReference)
+          framework_ref.name = framework
+          framework_ref.path = "Carthage/Build/iOS/" + framework
 
-  # project.targets.each do |target|
-  #   flagBuildPhase = false
-  #   target.build_phases.each do |build_phase|
-  #     if build_phase.isa == "PBXFrameworksBuildPhase"
-  #       frameworks["frameworks"].each do |framework|
-  #         framework_ref = project.new(Xcodeproj::Project::Object::PBXFileReference)
-  #         framework_ref.name = framework
-  #         framework_ref.path = "Carthage/Build/iOS/" + framework
-  #         build_phase.add_file_reference(framework_ref, true)
-  #       end
-  #     end
-  #   end
-  # end
-
-  # project.targets.each do |target|
-  #   target.build_phases.each do |build_phase|
-  #     if build_phase.isa != "PBXShellScriptBuildPhase"
-  #       next
-  #     elsif build_phase.name != BUILD_PHASE_NAME
-  #       next
-  #     end
-  #     frameworks["frameworks"].each do |framework|
-  #       framework_path = "$(SRCROOT)/Carthage/Build/iOS/" + framework
-  #       unless build_phase.input_paths.include?(framework_path)
-  #         build_phase.input_paths.push(framework_path)
-  #       end
-  #     end
-  #   end
-  # end
+          count = build_phase.files_references.count{|ref| ref.name == framework && framework_ref.path == ref.path }
+          if count == 0
+            build_phase.add_file_reference(framework_ref, false)
+          end
+        end
+      end
+    end
+  end
 
   project.save()
   
