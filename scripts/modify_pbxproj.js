807f0bc4b0e04e3877d2a1a692ad6ad094deafc3
nrikiji <nrikiji@gmail.com>
2019-01-16 22:56:00 +0900

    develop

--- scripts/modify_pbxproj.js
+++ scripts/modify_pbxproj.js
@@ -1,5 +1,6 @@
 #!/usr/bin/env node
 
+var exec = require("child_process").exec
 var execSync = require("child_process").execSync
 var fs = require("fs")
 var path = require("path")
@@ -9,40 +10,67 @@ var parser = new xml2js.Parser()
 var stdio = { stdio:[0, 1, 2] };
 
 module.exports = function (context) {
-    var rootPath = context.opts.projectRoot
-    var configPath = path.join(rootPath, "config.xml")
-    var configParser = getConfigParser(context, configPath)
-    var xcodeProjectPath = rootPath + "/platforms/ios/" + configParser.name() + ".xcodeproj"
+  var rootPath = context.opts.projectRoot
+  var configPath = path.join(rootPath, "config.xml")
+  var configParser = getConfigParser(context, configPath)
+  var platformPath = rootPath + "/platforms/ios"
+  var cartfilePath = platformPath + "/Cartfile"
+  var xcodeProjectPath = platformPath + "/" + configParser.name() + ".xcodeproj"
 
-    var frameworks = []
-    context.opts.cordova.plugins.forEach(id => {
-        parser.parseString(fs.readFileSync("plugins/" + id + "/plugin.xml"), (err, data) => {
-            if (!err && data.plugin.platform) {
-                data.plugin.platform.forEach((platform) => {
-                    if (platform.$.name === "ios") {
-                        (platform.framework || []).forEach(framework => {
-                            if (framework.$.carthage) {
-                                frameworks.push(configParser.name() + "/Plugins/" + id + "/" + path.basename(framework.$.src))
-                            }
-                        })
-                    }
+  var cartfiles = []
+  var frameworks = []
+
+  context.opts.cordova.plugins.forEach(id => {
+    parser.parseString(fs.readFileSync("plugins/" + id + "/plugin.xml"), (err, data) => {
+      if (!err && data.plugin.platform) {
+        data.plugin.platform.forEach((platform) => {
+          if (platform.$.name === "ios" && platform.carthage) {
+            platform.carthage.forEach((carthage) => {
+              if (carthage.cartfile) {
+                carthage.cartfile.forEach((cartfile) => {
+                  cartfiles.push(cartfile)
                 })
-            }
+              }
+              if (carthage.framework) {
+                carthage.framework.forEach((framework) => {
+                  if (framework.$ && framework.$.src) {
+                    frameworks.push(framework.$.src)
+                  }
+                })
+              }
+            })
+          }
         })
+      }
     })
-    if (0 < frameworks.length) {
-        var json = { frameworks: frameworks, project_path: xcodeProjectPath }
-        execSync("ruby " + __dirname + "/modify_pbxproj.rb '" + JSON.stringify(json) + "'", stdio);
-    }
+  })
 
-    function getConfigParser(context, config) {
-        var semver = context.requireCordovaModule("semver")
-        var ConfigParser
-        if (semver.lt(context.opts.cordova.version, "5.4.0")) {
-            ConfigParser = context.requireCordovaModule("cordova-lib/src/ConfigParser/ConfigParser")
-        } else {
-            ConfigParser = context.requireCordovaModule("cordova-common/src/ConfigParser/ConfigParser")
-        }
-        return new ConfigParser(config)
+  if (0 < cartfiles.length && 0 < frameworks.length) {
+    console.log("### Create Cartfile")
+    fs.writeFileSync(cartfilePath, cartfiles.join("\r\n"))
+
+    // var json = { frameworks: frameworks, project_path: xcodeProjectPath }
+    // execSync("ruby " + __dirname + "/modify_pbxproj.rb '" + JSON.stringify(json) + "'", stdio);
+
+    console.log("### Update Carthage")
+    exec("carthage update", { cwd: platformPath }, (err, stdout, stderr) => {
+      console.log(stdout)
+      console.log(stderr)
+
+      console.log("### Setting Run Script")
+      var json = { frameworks: frameworks, project_path: xcodeProjectPath }
+      execSync("ruby " + __dirname + "/modify_pbxproj.rb '" + JSON.stringify(json) + "'", stdio);
+    })
+  }
+
+  function getConfigParser(context, config) {
+    var semver = context.requireCordovaModule("semver")
+    var ConfigParser
+    if (semver.lt(context.opts.cordova.version, "5.4.0")) {
+      ConfigParser = context.requireCordovaModule("cordova-lib/src/ConfigParser/ConfigParser")
+    } else {
+      ConfigParser = context.requireCordovaModule("cordova-common/src/ConfigParser/ConfigParser")
     }
+    return new ConfigParser(config)
+  }
 }
--- scripts/modify_pbxproj.rb
+++ scripts/modify_pbxproj.rb
@@ -21,40 +21,70 @@ begin
   project = Xcodeproj::Project.open(project_path)
 
   project.targets.each do |target|
-    flagBuildPhase = false
-    target.build_phases.each do |build_phase|
-      if build_phase.isa != "PBXShellScriptBuildPhase"
-        next
-      elsif build_phase.name != BUILD_PHASE_NAME
-        next
-      else
-        flagBuildPhase = true
-      end
-    end
-    if !flagBuildPhase
-      build_phase = project.new(Xcodeproj::Project::Object::PBXShellScriptBuildPhase)
-      build_phase.name = BUILD_PHASE_NAME
-      build_phase.shell_path = "/bin/sh"
-      build_phase.shell_script = "/usr/local/bin/carthage copy-frameworks\n"
-      build_phase.input_paths = []
-      target.build_phases << build_phase
-    end
+    paths = ['$(inherited)', "$(PROJECT_DIR)/Carthage/Build/iOS"]
+    target.build_settings("Debug")["FRAMEWORK_SEARCH_PATHS"] = paths
+    target.build_settings("Release")["FRAMEWORK_SEARCH_PATHS"] = paths
   end
 
-  project.targets.each do |target|
-    target.build_phases.each do |build_phase|
-      if build_phase.isa != "PBXShellScriptBuildPhase"
-        next
-      elsif build_phase.name != BUILD_PHASE_NAME
-        next
-      end
-      frameworks["frameworks"].each do |framework|
-        unless build_phase.input_paths.include?(framework)
-          build_phase.input_paths.push(framework)
-        end
-      end
-    end
-  end
+  # project.build_configuration_list.build_configurations.each do |build_configuration|
+  #   p "###############################"
+  #   p build_configuration.isa
+  #   p build_configuration.name
+
+  #   paths = ["$(PROJECT_DIR)/Carthage/Build/iOS"]
+  #   build_configuration.build_settings["FRAMEWORK_SEARCH_PATHS"] = paths
+  # end
+
+  # project.targets.each do |target|
+  #   flagBuildPhase = false
+  #   target.build_phases.each do |build_phase|
+  #     if build_phase.isa != "PBXShellScriptBuildPhase"
+  #       next
+  #     elsif build_phase.name != BUILD_PHASE_NAME
+  #       next
+  #     else
+  #       flagBuildPhase = true
+  #     end
+  #   end
+  #   if !flagBuildPhase
+  #     build_phase = project.new(Xcodeproj::Project::Object::PBXShellScriptBuildPhase)
+  #     build_phase.name = BUILD_PHASE_NAME
+  #     build_phase.shell_path = "/bin/sh"
+  #     build_phase.shell_script = "/usr/local/bin/carthage copy-frameworks\n"
+  #     build_phase.input_paths = []
+  #     target.build_phases << build_phase
+  #   end
+  # end
+
+  # project.targets.each do |target|
+  #   flagBuildPhase = false
+  #   target.build_phases.each do |build_phase|
+  #     if build_phase.isa == "PBXFrameworksBuildPhase"
+  #       frameworks["frameworks"].each do |framework|
+  #         framework_ref = project.new(Xcodeproj::Project::Object::PBXFileReference)
+  #         framework_ref.name = framework
+  #         framework_ref.path = "Carthage/Build/iOS/" + framework
+  #         build_phase.add_file_reference(framework_ref, true)
+  #       end
+  #     end
+  #   end
+  # end
+
+  # project.targets.each do |target|
+  #   target.build_phases.each do |build_phase|
+  #     if build_phase.isa != "PBXShellScriptBuildPhase"
+  #       next
+  #     elsif build_phase.name != BUILD_PHASE_NAME
+  #       next
+  #     end
+  #     frameworks["frameworks"].each do |framework|
+  #       framework_path = "$(SRCROOT)/Carthage/Build/iOS/" + framework
+  #       unless build_phase.input_paths.include?(framework_path)
+  #         build_phase.input_paths.push(framework_path)
+  #       end
+  #     end
+  #   end
+  # end
 
   project.save()
   
